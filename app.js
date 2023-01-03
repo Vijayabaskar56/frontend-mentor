1fe5736593265e7de8e56f82b728b822913f6b5e
VIJAYA BASKAR <vj2k02@gmail.com>
2023-01-03 22:30:26 +0530

    get started with JS

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-function validateInput() {
-    // Get the input element
-    var input = document.getElementById("price");
+// Extract necessary elements from HTML
+const billInput = document.querySelector('input[name="price"]');
+const peopleInput = document.querySelector('input[name="people"]');
+const tipButtons = document.querySelectorAll('.btn');
+const tipAmountElement = document.querySelector('.output');
+const totalAmountElement = document.querySelector('#doller');
+
+
+// Set default values
+let tipPercentage = 0;
+let billAmount = 0;
+let peopleCount = 0;
+let totalAmount = 0;
+
+// Add event listeners
+billInput.addEventListener('input', updateAmounts);
+// peopleInput.addEventListener('input', updateAmounts);
+
+tipButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    tipPercentage = parseFloat(event.target.value);
+    updateAmounts();
+  });
+});
+
+// Define event handler functions
+function updateAmounts() {
+  billAmount = parseFloat(billInput.value);
+  peopleCount = parseInt(peopleInput.value);
   
-    // Only allow numbers
-    input.value = input.value.replace(/[^0-9.]/g, "");
-  }
+  const tipAmount = billAmount / tipPercentage;
+  const totalAmount = billAmount + tipAmount;
+  const amountPerPerson = totalAmount / peopleCount;
+  
+  tipAmountElement.textContent = tipAmount.toFixed(2);
+  totalAmountElement.textContent = totalAmount.toFixed(2);
+  totalAmountElement.nextElementSibling.textContent = `Reset`;
+}
+
+// Reset button
+document.querySelector('#btn-reset').addEventListener('click', event => {
+  billInput.value = 0;
+  peopleInput.value = 0;
+  tipPercentage = 0;
+  updateAmounts();
+});
+console.log(updateAmounts)
